6a44be75ff6d153bf5e35114e624ec7ba840c8ef
Sean <sdaoctavo@gmail.com>
2025-04-24 01:04:12 +0800

    Added global css and polished header component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import Home from './pages/Home.jsx';
 import Mapping from './pages/Mapping.jsx';
