e009c153ea54a03693bc3190e6247857473a3362
Sean <sdaoctavo@gmail.com>
2025-04-23 20:16:29 +0800

    Removed and fixed rendering logic redundancy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './pages/Home'
+import { Outlet } from 'react-router'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <Home />
-      <Mapping />
+      <Header />
+      <Outlet />
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './components/Header.jsx';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import Home from './pages/Home.jsx';
 import Mapping from './pages/Mapping.jsx';
 
-const Layout = () => {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
-  );
-};
-
 const router = createBrowserRouter([{
   path: '/',
-  element: <Layout />,
+  element: <App />,
   children: [
       {
         path:'/',
