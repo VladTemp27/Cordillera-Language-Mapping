1d861d0b22c4f325fac14b0cc14a7e7659e2d90f
Sean <sdaoctavo@gmail.com>
2025-04-23 20:02:29 +0800

    Setup routers for the landing page and language mapping page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Header from './components/Header.jsx';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router';
+import Home from './pages/Home.jsx';
+import Mapping from './pages/Mapping.jsx';
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout />,
+  children: [
+      {
+        path:'/',
+        element:<Home />
+      },
+      {
+        path:'/language-mapping',
+        element:<Mapping />
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
---
+++ frontend/src/pages/Mapping.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LanguageMapping from '../components/LanguageMapping'
+
+const Mapping = () => {
+  return (
+    <div>
+      <LanguageMapping />
+    </div>
+  )
+}
+
+export default Mapping
