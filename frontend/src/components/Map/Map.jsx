c97addd95e72fbfedef38a891a1efa845a985664
Sean <sdaoctavo@gmail.com>
2025-04-24 16:43:13 +0800

    Integrated react leaflet map in the page

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import './Map.css';
+import React from "react";
+import "./Map.css";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 const Map = () => {
   return (
-    <div className="map-container">
-      <div className="map-area">
-        <div className="map-region">
-          {/* Add your map shape here */}
-        </div>
-        <div className="map-label">
-          <span>City: Baguio City</span>
-        </div>
-      </div>
-    </div>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
   );
 };
 
--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import './SideBar.css';
-import SideBarTab from './SideBarTab';
-import SideBarContent from './SideBarContent';
+import React from "react";
+import "./SideBar.css";
+import SideBarTab from "./SideBarTab";
+import SideBarContent from "./SideBarContent";
 
 const SideBar = ({ children, activeTab, setActiveTab }) => {
   return (
-    <div className="sidebar">
+    <div>
       <div className="sidebar-title">
         <h1>CORDILLERA ADMINISTRATIVE REGION (CAR)</h1>
       </div>
-
       <SideBarTab activeTab={activeTab} setActiveTab={setActiveTab} />
       <SideBarContent>{children}</SideBarContent>
     </div>
