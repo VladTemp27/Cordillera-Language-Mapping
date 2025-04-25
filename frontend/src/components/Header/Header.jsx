a55764f7a13a2018dadaaada0ba09204b6732516
Sean <sdaoctavo@gmail.com>
2025-04-26 05:05:39 +0800

    Disabled searchbar, minor adjustments to header and culture component

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import './Header.css';
-import SearchBar from './SearchBar';
+import React from "react";
+import "../LandingPage/LandingPage.css";
+import baguioSeal from "../../assets/BaguioLogo.png";
 
 const Header = ({ setActiveTab }) => {
   return (
-    <div className="header">
-      <h1>Cordillera Administrative Region (CAR)</h1>
-      <SearchBar />
-    </div>
+      <header className="lp-header">
+        <div className="lp-header-content">
+          <div className="lp-header-title-container">
+            <img
+              src={baguioSeal}
+              alt="Baguio Logo"
+              className="lp-header-logo"
+            />
+            <div className="lp-header-text">
+              <span className="lp-header-sub">Republic of the Philippines</span>
+              <h1 className="lp-header-title">City Government of Baguio</h1>
+            </div>
+          </div>
+        </div>
+      </header>
   );
 };
 
