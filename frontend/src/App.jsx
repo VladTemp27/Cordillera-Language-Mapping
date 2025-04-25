0d06d2e03cb4ebea2e14a3139fd418e0f40c9e4d
Sean <sdaoctavo@gmail.com>
2025-04-25 20:23:56 +0800

    Header component is now only in mapping page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Header from './components/Header/Header'
 function App() {
   return (
     <div>
-      <Header />
       <Outlet />
     </div>
   )
--- frontend/src/pages/Mapping.jsx
+++ frontend/src/pages/Mapping.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import LanguageMapping from '../components/LanguageMapping/LanguageMapping'
+import Header from '../components/Header/Header'
 
 const Mapping = () => {
   return (
     <div>
+      <Header />
       <LanguageMapping />
     </div>
   )
