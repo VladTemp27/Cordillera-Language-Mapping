83d20951a1bb8034fe8947f212a7c8ee6f0f058d
Marven <luis.marvenjoffre@gmail.com>
2025-04-25 18:54:41 +0800

    Refactor province routes to format response data with ethnic groups and languages

--- server/rest-api/routes/provinceRoutes.js
+++ server/rest-api/routes/provinceRoutes.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const { getAllProvinces , getProvinceById } = require("../services/dal-service/data-service/ProvinceDAL");
+const { getAllProvinces, getProvinceById } = require("../services/dal-service/data-service/ProvinceDAL");
+const { getEthnicGroupsByProvinceId } = require("../services/dal-service/data-service/EthnicGroupDAL");
+const { getLanguagesByProvinceId } = require("../services/dal-service/data-service/LanguageDAL");
 
 const router = express.Router();
 
 router.get('/getAll', async (req, res) => {
     try {
         const provinces = await getAllProvinces();
-        res.json(provinces);
+        const formattedProvinces = {
+            provinces: await Promise.all(provinces.map(async (province) => {
+                const ethnicGroups = await getEthnicGroupsByProvinceId(province.id);
+                const languages = await getLanguagesByProvinceId(province.id);
+                return {
+                    name: province.name,
+                    history: province.history,
+                    ethnicGroups: ethnicGroups.map(eg => eg.name),
+                    languages: languages.map(lang => lang.name)
+                };
+            }))
+        };
+        res.json(formattedProvinces);
     } catch (error) {
         res.status(500).json({ error: error.message });
-
     }
 });
-
 
 router.get('/province/:id', async (req, res) => {
     try {
@@ -21,8 +33,20 @@ router.get('/province/:id', async (req, res) => {
             return res.status(400).json({ error: 'Invalid province ID' });
         }
         
-        const provinces = await getProvinceById(provinceId);
-        res.json(provinces);
+        const province = await getProvinceById(provinceId);
+        const ethnicGroups = await getEthnicGroupsByProvinceId(provinceId);
+        const languages = await getLanguagesByProvinceId(provinceId);
+        
+        const formattedResponse = {
+            provinces: [{
+                name: province.name,
+                history: province.history,
+                ethnicGroups: ethnicGroups.map(eg => eg.name),
+                languages: languages.map(lang => lang.name)
+            }]
+        };
+        
+        res.json(formattedResponse);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
