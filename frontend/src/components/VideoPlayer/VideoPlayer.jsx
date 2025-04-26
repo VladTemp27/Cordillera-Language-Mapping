c4862be22c9450f4cba063f5db67a0bf9d97c302
Sean <sdaoctavo@gmail.com>
2025-04-26 21:55:25 +0800

    Used EmblaCarousel for the VideoPlayers

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import "./VideoPlayer.css";
+import EmblaCarousel from "./EmblaCarousel/EmblaCarousel";
+
+const OPTIONS = { loop: true }
+const VIDEO_URLS = [
+  "https://www.youtube.com/watch?v=0QO88x3lJVc", 
+  "https://www.youtube.com/watch?v=L1nup3Y5iOw",
+  ""
+];
 
 const VideoPlayer = () => {
   return (
     <div className="video-container">
-      <h2>This is Baguio City</h2>
-      <ReactPlayer url={"https://www.youtube.com/watch?v=L1nup3Y5iOw"} />
+      <h1>Explore Baguio City</h1>
+      <EmblaCarousel slides={VIDEO_URLS} options={OPTIONS} />
     </div>
   );
 };
